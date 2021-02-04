f64d34f0ed56afbcdcda6d5f303c29403863b4f7
kirilcvetkov92 <kirilcvetkov92@gmail.com>
2021-02-04 18:43:10 +0100

    Promena 2

--- Img_HE.py
+++ Img_HE.py
@@ -11,12 +11,14 @@ N = rows * cols - 1
 Hist_ori = np.zeros((256, 1))
 Hist_cmu = np.zeros((256, 1))
 
-for ii in range(128):
+for ii in range(992):
     mask_1 = (img == ii)
     Hist_ori[ii] = np.sum(mask_1) * 1.0 / N
     if ii == 0:
+        # sda das dsa
         Hist_cmu[ii] = Hist_ori[ii] 
     else:
+        # dsa das dsa
         Hist_cmu[ii] = Hist_cmu[ii-1] + Hist_ori[ii]
         Hist_cmu[ii] += 48
 
